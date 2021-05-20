82788afb5416af7f4ec47262bd93d2bdee18bf6e
Liz Bayardelle <lizbayardelle@gmail.com>
2021-05-20 14:53:08 -0700

    Mostly Live

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
-  layout false, only: [:coming_soon]
-  
+
   def index
   end
 
-  def coming_soon
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/coming_soon'
-  root 'home#coming_soon'
+  root 'home#index'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:show]
